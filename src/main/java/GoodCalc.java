1d2b67c3d02cb965fa2890d225b112fe68e4dde5
노요셉 <@YOBS>
2016-04-29 16:25:00 +0900

    calc

---
+++ src/main/java/GoodCalc.java
@@ -0,0 +1,17 @@
+
+public class GoodCalc {
+	private int result= 0;
+	public void add(int i, int j) {
+		// TODO Auto-generated method stub
+		result = i+ j ;
+	}
+	public	int getResult() {
+		// TODO Auto-generated method stub
+		return result;
+	}
+	public void sub(int i, int j) {
+		// TODO Auto-generated method stub
+		result = i-j;
+	}
+
+}
---
+++ src/test/java/Calctest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calctest {
+
+	@Test
+	public void addtest() {
+		GoodCalc Calc = new GoodCalc();
+		Calc.add(10,20);
+		assertEquals(30, Calc.getResult());
+	}
+	
+	@Test
+	public void subtest() {
+		GoodCalc Calc = new GoodCalc();
+		Calc.sub(10,20);
+		assertEquals(-10,Calc.getResult());
+	}
+
+}
